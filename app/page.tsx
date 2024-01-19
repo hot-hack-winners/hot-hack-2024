c6fc5abb634d73b7d68f72e9d28a39667721a7c3
Tylor Stewart <trjstewart@gmail.com>
2024-01-19 22:24:13 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          Get started by&nbsp;
+          <code className={styles.code}>bribing the judges</code>
         </p>
         <div>
           <a
