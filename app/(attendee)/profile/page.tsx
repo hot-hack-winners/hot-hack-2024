0dddc739fd6b8cdb718b21a01ef802c210f3b845
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2024-01-20 11:56:15 +1000

    s

---
+++ app/(attendee)/profile/page.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+
+import {DashboardComponent} from '@/components/DashboardComponent'
+
+
+export default function Page() {
+
+
+    return <DashboardComponent/>
+
+}
--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   useEffect(() => {
     completeLogin()
       .then(() => {
-        Router.push('/dashboard')
+        Router.push('/profile')
       })
       .catch((error) => {
         console.error(error)
