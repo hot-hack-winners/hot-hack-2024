03eebb865dea9388c00055fdd410b1a9c1ba3367
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2024-01-20 17:32:01 +1000

    s

---
+++ app/(attendee)/scan/[venueUuid]/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import {ScanDashboard} from '@/components/ScanDashboard'
+
+
+
+export default function Page() {
+
+
+    return <ScanDashboard playlistId={'12b8Y3MRyU6TX8D3a8LdAJ'} />
+}
