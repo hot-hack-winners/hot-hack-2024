833a41b6c6fc74fa72bb181a1521dac49b581581
Luke Mai <mailong.c8.cva@gmail.com>
2024-01-20 17:16:13 +1000

    Adding theme

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,10 +2,11 @@
 
 import VenuesDashboard from '@/components/VenuesDashboard'
 
-
+import { useTheme } from "next-themes"
 
 export default function Page() {
-
+    const { setTheme } = useTheme()
+    setTheme("dark")
 
     return <VenuesDashboard/>
 
--- components/VenuesDashboard.tsx
+++ components/VenuesDashboard.tsx
@@ -1,19 +1,37 @@
+import { Calendar } from "@/components/ui/calendar"
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 
 export default function VenuesDashboard() {
+    const days: Date[] = [new Date("2024-01-20"),new Date("2024-01-21"),new Date("2024-01-19")]
   return (
     <div>
-      <Card>
+      <Card> 
         <div>
+            
           <div>
             <div>Name of Venues</div>
             <Button>Create gig</Button>
           </div>
-          <div>Gig list</div>
+          <div>
+            <div>
+                <Calendar
+                    mode="multiple"
+                    selected={days}
+                />
+            
+            </div>
+           
+            <div>
+                Gig list
+            </div>
+
+          </div>
+          
           <div>Suggested Artist</div>
+          
         </div>
-      </Card>
+        </Card> 
     </div>
   );
 }
