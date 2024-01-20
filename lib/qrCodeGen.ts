d110d792c3a0e0270ba2dd35d8c96eb25da45184
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2024-01-20 22:05:46 +1000

    Update qrCodeGen.ts

--- lib/qrCodeGen.ts
+++ lib/qrCodeGen.ts
@@ -2,15 +2,15 @@ import * as QRCode from "qrcode";
 
 const URL = process.env.NEXT_PUBLIC_HOST_NAME;
 
-export async function generateQR(venueId: string) {
+export async function generateQR(venueId: string): Promise<string | null> {
     if (URL) {
         const scanURL = URL.concat("/scan/").concat(venueId);
 
         try {
             const qrcode = await QRCode.toDataURL(scanURL, {
                 color: {
-                    dark: "#000",
-                    light: "#ffff"  // This should be "#ffffff" or "transparent"
+                    dark: "#000000",  // For black color
+                    light: "#ffffff"  // For white background, or "transparent"
                 }
             });
             console.log('QR Code Generated:', qrcode);
