01deaab6bae4a8df99484636b0cd1627b777f4a1
dlake2 <82156664+lake-d1@users.noreply.github.com>
2024-01-21 12:13:17 +1000

    Fixed submit function

--- server/attendees.ts
+++ server/attendees.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt';
 
 const attendeeSchema = z.object(
     {
-        uuid: z.string().optional(),
+        uuid: z.string(),
         spotify_id: z.string().optional(),
         name: z.string(),
         email: z.string(),
@@ -28,7 +28,14 @@ export async function getAttendeeByID(attendeeId: string) {
     )
 }
 
-export async function addAttendee(attendee: Attendee) {
+export async function getAttendeeBySpotifyID(spotify_id: string) {
+    return await executeQuery<Attendee>(
+        'SELECT * FROM attendees where spotify_id = ?;',
+        [spotify_id]
+    )
+}
+
+export async function addAttendee(attendee: Saveable<Attendee>) {
     return await executeQuery(
         'INSERT INTO attendees (uuid, spotify_id, name, email) VALUES(uuid(), ?, ?, ?)',
         [attendee.spotify_id, attendee.name, attendee.email]
